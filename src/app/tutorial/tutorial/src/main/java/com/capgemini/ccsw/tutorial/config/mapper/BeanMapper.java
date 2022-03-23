8ec6a4c1ad4eac0a8fa4a9392a5f6bdf2b373c08
nestelac <100681775+nestelac@users.noreply.github.com>
2022-03-23 11:01:26 +0100

    Update BeanMapper.java

--- src/app/tutorial/tutorial/src/main/java/com/capgemini/ccsw/tutorial/config/mapper/BeanMapper.java
+++ src/app/tutorial/tutorial/src/main/java/com/capgemini/ccsw/tutorial/config/mapper/BeanMapper.java
@@ -2,27 +2,18 @@ package com.capgemini.ccsw.tutorial.config.mapper;
 
 import org.springframework.data.domain.Page;
 
-import com.capgemini.ccsw.tutorial.author.model.Author;
-import com.capgemini.ccsw.tutorial.author.model.AuthorDto;
-import com.capgemini.ccsw.tutorial.prestamo.model.Prestamo;
-import com.capgemini.ccsw.tutorial.prestamo.model.PrestamoDto;
+/**
+* @author coedevon
+*/
+public interface BeanMapper extends com.devonfw.module.beanmapping.common.api.BeanMapper {
 
-public class BeanMapper {
-
-	public Page<AuthorDto> mapPage1(Page<Author> findPage, Class<AuthorDto> class1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Page<AuthorDto> mapPage(Page<Author> page, Class<AuthorDto> class1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public Page<PrestamoDto> mapPage2(Page<Prestamo> findPage, Class<PrestamoDto> class1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+   /**
+   * Mapea el genérico de un Page en otro tipo de genérico
+   * @param <T>
+   * @param source
+   * @param targetClass
+   * @return
+   */
+   <T> Page<T> mapPage(Page<?> source, Class<T> targetClass);
 
 }
